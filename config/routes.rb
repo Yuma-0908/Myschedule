5f21b5c8b9d7f026546d6e1d95ac79f505b39d23
EC2 Default User <ec2-user@ip-172-31-9-129.ap-southeast-1.compute.internal>
2022-05-21 20:02:40 +0900

    コメントモデルとコメントコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-    resources :todos
+    resources :todos do
+      resources :comments
+    end
     
     root 'todos#index'
     
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220429122624) do
+ActiveRecord::Schema.define(version: 20220521105801) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "todo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_comments_on_todo_id"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
