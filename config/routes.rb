5ff26ba3058a5ed3faff68e18541be8c4547e0d7
EC2 Default User <ec2-user@ip-172-31-9-129.ap-southeast-1.compute.internal>
2022-05-27 17:29:15 +0900

    コメント最終調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
     resources :todos do
-      resources :comments
+      resources :comments, only: [:create, :destroy
+      ]
     end
     
     root 'todos#index'
